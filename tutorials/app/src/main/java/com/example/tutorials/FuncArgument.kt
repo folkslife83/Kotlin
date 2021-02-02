42b1de12bbc919dbbca0c0f4eaf5f34f44e09414
folkslife83 <folkslife@naver.com>
2021-02-02 17:04:57 +0900

    add FuncArg

---
+++ tutorials/app/src/main/java/com/example/tutorials/FuncArgument.kt
@@ -0,0 +1,10 @@
+package com.example.tutorials //중복된 함수 이름때문에 에러날경우 .anyname 추가
+
+fun sum(a: Int, b: Int) = a + b
+fun mul(a: Int, b: Int) = a * b
+
+fun main() {
+    val result = sum(10, 3)
+    val result2 = mul(sum(3,3), 2)
+    println("result: $result, result2: $result2")
+}
